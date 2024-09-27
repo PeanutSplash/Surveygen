8015069322cf3383ed5ce2c1d7f80f69945eafd6
Jan <b1300658700@outlook.com>
2024-09-28 02:35:00 +0800

    fix:修复页面不可点击

--- src/components/FloatingWindow.vue
+++ src/components/FloatingWindow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="surveyStore.isVisible" class="!fixed !inset-0 !z-[9999]">
+  <div v-if="surveyStore.isVisible" class="!fixed !inset-0 !z-[9999] pointer-events-none">
     <vue-draggable-resizable
       :w="600"
       :h="400"
